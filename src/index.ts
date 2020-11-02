afb1b42e281263397ccd8505418e00bdf5ffbb11
André <andrevictor50@gmail.com>
2020-11-02 16:23:34 -0300

    configuring orm

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
-import { Post } from './entities/Post';
 import mikroOrmConfig from './mikro-orm.config';
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
 
-  orm.em.create(Post, {
-    title: 'My post',
-  });
+  await orm.getMigrator().up();
 }
 
 main().catch(error => {
--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,8 +1,13 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
+import path from 'path';
 
 export default {
+  migrations: {
+    path: path.join(__dirname, './migrations'),
+    pattern: /^[\w-]+\d+\.[tj]s$/,
+  },
   entities: [Post],
   dbName: 'reddit',
   type: 'postgresql',
