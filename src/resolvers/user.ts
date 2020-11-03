3e05b88216c81f60c17604f8e118a253a9a239c1
André <andrevictor50@gmail.com>
2020-11-03 16:36:26 -0300

    logging user

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -6,6 +6,7 @@ import { Arg,
   Field,
   InputType,
   Mutation,
+  ObjectType,
   Resolver,
 } from "type-graphql";
 
@@ -18,13 +19,29 @@ class UsernamePasswordInput {
   password: string;
 }
 
+@ObjectType()
+class FieldError {
+  @Field()
+  message: string;
+}
+
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[]
+
+  @Field(() => User, { nullable: true })
+  user?: User
+}
+
 @Resolver()
 export class UserResolver {
   @Mutation(() => User)
   async register(
     @Arg('options') options: UsernamePasswordInput,
     @Ctx() { em } : MyContext
-  ) {
+  ): Promise<User> {
     const { username, password } = options;
 
     const hashedPassword = await argon2.hash(password);
@@ -37,5 +54,39 @@ export class UserResolver {
     await em.persistAndFlush(user);
 
     return user;
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg('options') options: UsernamePasswordInput,
+    @Ctx() { em } : MyContext
+  ): Promise<UserResponse> {
+    const { username, password } = options;
+
+    const user = await em.findOne(User, {
+      username,
+    });
+
+    if (!user) {
+      return {
+        errors: [{
+          message: 'Incorrect username/password',
+        }]
+      }
+    }
+
+    const valid = await argon2.verify(user.password, password);
+
+    if (!valid) {
+      return {
+        errors: [{
+          message: 'Incorrect username/password',
+        }]
+      }
+    }
+
+    return {
+      user,
+    };
   }
 }
