2e75c399af652790feaffc6630ed60f3359c8475
André <andrevictor50@gmail.com>
2020-11-04 10:13:59 -0300

    correct url

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ const main = async () => {
   const app = express();
 
   app.use(cors({
-    origin: 'http://localhost3000',
+    origin: 'http://localhost:3000',
     credentials: true,
   }));
 
