fbda257b0044fc42a13a37fda715ff5c99bd35dd
André <andrevictor50@gmail.com>
2020-11-04 07:50:32 -0300

    index file on resolvers

---
+++ src/resolvers/index.ts
@@ -0,0 +1,11 @@
+import { HelloResolver } from './hello';
+import { PostResolver } from './post';
+import { UserResolver } from './user';
+
+const resolvers = [
+  HelloResolver,
+  PostResolver,
+  UserResolver,
+];
+
+export default resolvers;
